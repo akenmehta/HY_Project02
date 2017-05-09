3b5a5dc484988119a6be1c2e7a49c4a24f7cd1db
mehta-aken <mehta.aken@gmail.com>
2017-05-09 15:19:23 -0400

    added google maps

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ $(window).resize(function(){
 	else
 		$('.nav-container').removeClass('shrink');
 });
+
+function initMap() {
+  var uluru = {lat: 43.648264, lng: -79.397859};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
