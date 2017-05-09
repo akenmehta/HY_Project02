019a15b3a7c175f763c1b0712b79fa8ecfe88ad3
mehta-aken <mehta.aken@gmail.com>
2017-05-09 18:57:05 -0400

    fixed nav and map display issue

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-$(window).scroll(function(){
-	if($(document).scrollTop()>60)
-		$('.nav-container').addClass('shrink');
-	else
-		$('.nav-container').removeClass('shrink');
-});
-
+// google maps code
+function initMap() {
+    var uluru = {lat: 43.648, lng: -79.397};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 18,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+}
+initMap();
+// add nav bar when window resizes
 $(window).resize(function(){
 	if($(window).width() < 960)
 		$('.nav-container').addClass('shrink');
-	else
+});
+
+// add/remove nav bar based on scroll and window size
+$(window).scroll(function(){
+	if($(document).scrollTop()>60)
+		$('.nav-container').addClass('shrink');
+	else if($(window).width() > 960)
 		$('.nav-container').removeClass('shrink');
+
+	$('header nav').removeClass('nav-open');
 });
 
 $('.burger-nav').on('click', function(){
 	$('header nav').toggleClass('nav-open');
 });
-
-function initMap() {
-  var uluru = {lat: 43.648264, lng: -79.397859};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 18,
-    center: uluru
-  });
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
-  });
-}
