0a17aa8a5071c1bed1a8775df8191fb1340c6859
mehta-aken <mehta.aken@gmail.com>
2017-05-10 08:40:40 -0400

    modified js file to include document.ready()

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-// add nav bar when window resizes
-$(window).resize(function(){
-	if($(window).width() < 960)
-		$('.nav-container').addClass('shrink');
+$(document).ready(function(){
+
+	// add nav bar when window resizes
+	$(window).resize(function(){
+		if($(window).width() < 960)
+			$('.nav-container').addClass('shrink');
+	});
+
+	// add/remove nav bar based on scroll and window size
+	$(window).scroll(function(){
+		if($(document).scrollTop()>60)
+			$('.nav-container').addClass('shrink');
+		else if($(window).width() > 960)
+			$('.nav-container').removeClass('shrink');
+
+		$('header nav').removeClass('nav-open');
+	});
+
+	$('.burger-nav').on('click', function(){
+		$('header nav').toggleClass('nav-open');
+	});
+
+	// google maps code
+	function initMap() {
+	    var uluru = {lat: 43.648, lng: -79.397};
+	    var map = new google.maps.Map(document.getElementById('map'), {
+	      zoom: 18,
+	      center: uluru
+	    });
+	    var marker = new google.maps.Marker({
+	      position: uluru,
+	      map: map
+	    });
+	}
+	initMap();
+	
 });
-
-// add/remove nav bar based on scroll and window size
-$(window).scroll(function(){
-	if($(document).scrollTop()>60)
-		$('.nav-container').addClass('shrink');
-	else if($(window).width() > 960)
-		$('.nav-container').removeClass('shrink');
-
-	$('header nav').removeClass('nav-open');
-});
-
-$('.burger-nav').on('click', function(){
-	$('header nav').toggleClass('nav-open');
-});
-
-// google maps code
-function initMap() {
-    var uluru = {lat: 43.648, lng: -79.397};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 18,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-}
-initMap();
